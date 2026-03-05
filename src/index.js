d54e315a53a3872b1b97494baa5c858c04cc6646
yujikawa <ykyujikawa@gmail.com>
2026-03-05 16:23:30 +0900

    v1.0.1

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const program = new Command();
 program
   .name('modscape')
   .description('Modscape: A YAML-driven data modeling visualizer CLI')
-  .version('1.0.0');
+  .version('1.0.1');
 
 program
   .command('init')
