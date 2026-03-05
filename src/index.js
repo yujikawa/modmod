d7886dd517a058d57f0d886745209ae514922055
yujikawa <ykyujikawa@gmail.com>
2026-03-05 18:06:26 +0900

    chore: bump version to v1.0.2

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const program = new Command();
 program
   .name('modscape')
   .description('Modscape: A YAML-driven data modeling visualizer CLI')
-  .version('1.0.1');
+  .version('1.0.2');
 
 program
   .command('init')
